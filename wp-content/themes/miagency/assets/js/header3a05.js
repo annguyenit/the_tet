98ba2fa1466ef1a69ccfcfe9871d4cc4d2101777
annguyenit <annguyenphp@gmail.com>
2015-07-30 02:01:31 +0700

    Update rototo font, Add flags language

--- wp-content/themes/miagency/assets/js/header3a05.js
+++ wp-content/themes/miagency/assets/js/header3a05.js
@@ -143,7 +143,7 @@
 
     function onScroll(event){
         var scrollPos = $(document).scrollTop();
-        $('#menu-mainmenu a').each(function () {
+        $('#menu-mainmenu a[href^="#"]').each(function () {
             var currLink = $(this);
             var refElement = $(currLink.attr("href"));
             if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
