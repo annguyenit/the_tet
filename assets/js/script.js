37efcb08823d1300143f8d2b15a753984c8f32d2
quan.tp <quan.tp@sutrixmedia.com>
2015-07-31 15:38:05 +0700

    Update fix issue project block + scroll for question.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,35 @@
 ;(function($, window, undefined) {
-	var list = {}
-	var getPro = function() {
-		var i = 0;
-		$('#project').find('.project').each(function() {
-			var that = this;
-			list['project' + i] =  function() {
-				return $(that).offset().top;
-			}
-			i++;
-		});
-	};
-	getPro();
-	var s = skrollr.init({
-		constants: list
-	});
+	var s = skrollr.init();
 	$(window).on('resize', function() {
 		s.refresh();
 	});
+		// $(document).on("scroll.scoll-2", onScroll1);
+  //   $('.flex-control-nav a[href^="#"]').on('click', function (e) {
+  //       e.preventDefault();
+  //       $(document).off("scroll.scroll-2");
+
+  //       $('.flex-control-nav a').removeClass('flex-active');
+  //       $(this).addClass('flex-active');
+
+  //       var $anchor = $(this);
+  //       $('html, body').stop().animate({
+  //           scrollTop: $($anchor.attr('href')).offset().top
+  //       }, 800, 'easeInOutExpo', function () {
+  //           $(document).on("scroll.scoll-2", onScroll1);
+  //       });
+  //   });
+  //   function onScroll1(event){
+  //       var scrollPos = $(document).scrollTop();
+  //       $('.flex-control-nav a[href^="#"]').each(function () {
+  //           var currLink = $(this);
+  //           var refElement = $(currLink.attr("href")).next();
+  //           var refPost = refElement.position().top;
+  //           if (refPost <= scrollPos && refPost + refElement.height() > scrollPos) {
+  //               $('.flex-control-nav a').removeClass("flex-active");
+  //               currLink.addClass("flex-active");
+  //           } else {
+  //               currLink.removeClass("active");
+  //           }
+  //       });
+  //   }
 }(jQuery, window));
